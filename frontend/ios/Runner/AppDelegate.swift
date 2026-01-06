ea97d413690be121679db11dcfe0f14d82c0d1a0
김수한 <kimsuhan@gimsuhan-ui-MacBookAir.local>
2026-01-06 16:03:57 +0900

    아이콘 최종 수정 완료

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import Firebase
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +9,18 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // 1. Firebase 초기화 (GoogleService-Info.plist 설정 적용)
+    if FirebaseApp.app() == nil {
+        FirebaseApp.configure()
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // 2. APNs 토큰이 생성되었을 때 Firebase Messaging에 전달하여 매칭시키는 메서드
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
   }
 }
